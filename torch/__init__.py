408831172f4ba0b5caedd13bb07c30850a382f4e
Tom Ritchford <tom@swirly.com>
2024-07-09 17:59:47 +0000

    Fix #121334: graph break on constant method call
    ghstack-source-id: 8ef320af20796b1b305ca914fda50b5d9aebf7ef
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/130158

--- torch/__init__.py
+++ torch/__init__.py
@@ -510,12 +510,24 @@ class SymInt:
     def _sympy_(self):
         return self.node.expr
 
-    def __hash__(self) -> builtins.int:
+    def _get_int(self) -> builtins.int:
         if self.node.is_nested_int():
-            return hash(self.node.nested_int())
+            return self.node.nested_int()
         else:
-            # Force specialization
-            return hash(builtins.int(self))
+            return builtins.int(self)
+
+    def __hash__(self) -> builtins.int:
+        return hash(self._get_int())
+
+    def as_integer_ratio(self) -> _Tuple[builtins.int, builtins.int]:
+        """Represent this int as an exact integer ratio"""
+        return self._get_int(), 1
+
+    def bit_length(self) -> "SymInt":
+        return sym_int(self._get_int().bit_length())
+
+    def conjugate(self) -> "SymInt":
+        return self
 
 
 class SymFloat:
@@ -615,18 +627,21 @@ class SymFloat:
         """Return True if the float is an integer."""
         raise TypeError("type stub not overridden")
 
+    def as_integer_ratio(self) -> _Tuple[builtins.int, builtins.int]:
+        """Represent this float as an exact integer ratio"""
+        return self._get_float().as_integer_ratio()
+
     def __repr__(self):
         return self.node.str()
 
     def _sympy_(self):
         return self.node.expr
 
+    def _get_float(self) -> builtins.float:
+        return self.node.float_() if self.node.is_constant() else builtins.float(self)
+
     def __hash__(self):
-        if self.node.is_constant():
-            return hash(self.node.float_())
-        else:
-            # Force specialization
-            return hash(builtins.float(self))
+        return hash(self._get_float())
 
 
 class SymBool:
